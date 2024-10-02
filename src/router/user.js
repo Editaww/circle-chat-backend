230412d01d919fb2de2c71ddc2d65574d25983d4
Editaww <151859796+Editaww@users.noreply.github.com>
2024-10-02 21:19:01 +0300

    improved

--- src/router/user.js
+++ src/router/user.js
@@ -1,24 +1,10 @@
-// import express from "express";
-
-// import { LOGIN, REGISTER } from "../controller/user.js";
-
-// const router = express.Router();
-// router.post("/register", REGISTER);
-// router.post("/login", LOGIN);
-
-// export default router;
-
 import express from "express";
 import { LOGIN, REGISTER } from "../controller/user.js";
-import auth from "../middleware/auth.js"; // Importuok autentifikacijos middleware
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/register", REGISTER);
 router.post("/login", LOGIN);
-// router.get("/login/validate", auth, (req, res) => {
-//   // Jei žetonas galioja, šis maršrutas grąžins sėkmės pranešimą
-//   return res.status(200).json({ message: "Token is valid" });
-// });
 
 export default router;
